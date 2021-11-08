828ab113acee78c9761ccaefcb0c3e1fb48a0e3d
Brandon Pena <brandonpena@gmail.com>
2021-11-08 16:14:37 -0500

    Capture user input with command-line arguments 2

--- App.js
+++ App.js
@@ -3,10 +3,16 @@ console.log(profileDataArgs);
 
 
 const printProfileData = profileDataArr => {
+    // This...
     for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-        
+        console.log(profileDataArr[i]);  
     }
+
+
+    console.log('===============');
+
+    // Is the same as this..
+    profileDataArr.forEach(profileItem => console.log(profileItem));
   };
   
   printProfileData(profileDataArgs);
